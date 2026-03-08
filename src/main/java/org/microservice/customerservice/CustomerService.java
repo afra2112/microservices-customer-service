2fa984ccf432f92b27f474bad8b5bda604e45159
afra65069@gmail.com <andres.ramirez01@globant.com>
2026-03-08 10:15:34 -0500

    put mapping implemented

--- src/main/java/org/microservice/customerservice/CustomerService.java
+++ src/main/java/org/microservice/customerservice/CustomerService.java
@@ -1,6 +1,7 @@
 package org.microservice.customerservice;
 
 import lombok.RequiredArgsConstructor;
+import org.microservice.customerservice.exception.CustomerNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,5 +16,23 @@ public class CustomerService {
     UUID createCustomer(CustomerRequest request){
         var customer = customerRepository.save(customerMapper.requestToEntity(request));
         return customer.getCustomerId();
+    }
+
+    void updateCustomer(CustomerRequest request){
+        var customer = customerRepository.findById(request.customerId()).orElseThrow(
+                () -> new CustomerNotFoundException(
+                        String.format("Cannot update customer:: No customer found with the provided ID:: %s", request.customerId())
+                )
+        );
+
+        mergeCustomer(customer, request);
+        customerRepository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest request){
+        customer.setFirstname(request.firstname());
+        customer.setLastname(request.lastname());
+        customer.setEmail(request.email());
+        customer.setAddress(request.address());
     }
 }
