a5e30b3be6aad2ca9bae4a95c094e3f6f64df351
afra65069@gmail.com <andres.ramirez01@globant.com>
2026-03-25 20:57:32 -0500

    pom fix

--- src/main/java/org/microservice/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/microservice/customerservice/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package org.microservice.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
